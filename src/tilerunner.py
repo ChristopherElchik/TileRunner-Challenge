7937eb570c97ea478c3f2fcda696302489f62c83
cwelchik <cwelchik@ncsu.edu>
2024-06-23 21:05:17 -0400

    merged main() from gpt version

--- src/tilerunner.py
+++ src/tilerunner.py
@@ -9,16 +9,12 @@ VISUALIZE = True
 
 # This is the file of the map to be used. You can change this to
 # use any map you want. Feel free to test your robot on custom maps!
+#
+# Note: Your robot will ALWAYS start in the top left corner.
 MAP_FILE = "MAP_01.txt"
 
-# options for window size
-SMALL_WINDOW = 0
-NORMAL_WINDOW = 1
-BIG_WINDOW = 2
-
-# Set the simulation's window size to either BIG_WINDOW, NORMAL_WINDOW
-# or SMALL_WINDOW. Default is NORMAL_WINDOW.
-WINDOW_SIZE = NORMAL_WINDOW
+# Size of each tile in pixels (Feel free to adjust to fit your screen)
+TILE_SIZE = 50
 
 # constants to define the robot's movement. Your nextMove() function
 # MUST return one of these. DO NOT change these values!
@@ -154,9 +150,10 @@ class Environment:
 
 
 class Robot:
-    def __init__(self, env):
+    def __init__(self, grid, env: Environment, pos: Tile):
+        self.grid = grid
         self.env = env
-        self.pos = 
+        self.pos = pos
         self.x = 0
         self.y = 0
         self.visited = set()
@@ -183,6 +180,49 @@ class Robot:
             self.visited.add((self.x, self.y))
         self.grid.update()
 
+class Grid:
+    def __init__(self, master, env: Environment):
+        self.master = master
+        self.env = env
+        self.canvas = tk.Canvas(master, width=len(env.map[0])*TILE_SIZE,
+                                height=len(env.map)*TILE_SIZE)
+        self.canvas.pack()
+        self.walls = set()
+        self.robot = Robot(self, env[0][0]) # sets starting point in top left corner
+        self.create_walls()
+        self.update()
+
+    def create_walls(self):
+        # Randomly create walls
+        # for _ in range(15):
+        #     x, y = random.randint(0, GRID_SIZE-1), random.randint(0, GRID_SIZE-1)
+        #     self.walls.add((x, y))
+        pass
+
+    def update(self):
+        self.canvas.delete('all')
+        for x in range(len(self.env.map[0])):
+            for y in range(len(self.env.map)):
+                color = 'white'
+                if (x, y) in self.walls:
+                    color = 'black'
+                elif (x, y) == (self.robot.x, self.robot.y):
+                    color = 'red'
+                elif (x, y) in self.robot.visited:
+                    color = 'green'
+                self.canvas.create_rectangle(y*TILE_SIZE, x*TILE_SIZE,
+                                             (y+1)*TILE_SIZE, (x+1)*TILE_SIZE,
+                                             fill=color)
+
 
 def main():
-    pass
+    root = tk.Tk()
+    root.title("TileRunner Challenge")
+    env = Environment()
+    grid = Grid(root, env)
+    root.after(500, grid.run)
+    root.mainloop()
+    
+
+if __name__ == '__main__':
+    main()
